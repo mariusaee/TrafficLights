6ba23cfe28f35ced82761aadb9b5708ae6aafdf0
mariusaee <mariusae@me.com>
2021-01-26 22:51:15 +0300

    Outlets created

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
